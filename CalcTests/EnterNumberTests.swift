bc02c0252a5f8d68b5113bf041113f40a063d63e
Matthew Paul Harding <matthewpharding@gmail.com>
2022-01-26 16:56:35 +0000

    Created a new unit test class to test the number input. Havent started the code yet

---
+++ CalcTests/EnterNumberTests.swift
@@ -0,0 +1,26 @@
+//
+//  EnterNumberTests.swift
+//  CalcTests
+//
+//  Created by Matthew Paul Harding on 26/01/2022.
+//
+//
+//
+//  iOSB Free Ltd                   → All rights reserved
+//  Website                         → https://www.iosbfree.com
+//
+//  👉 Free Courses                 → https://www.udemy.com/user/iosbfree
+//
+//  YouTube                         → https://www.youtube.com/channel/UCWBUOVRbtKNml4jN_4bRkCQ
+//  Linked In                       → http://www.linkedin.com/in/mattharding-iosbfree
+//  Tell us what you want to learn  → community@iosbfree.com
+// *******************************************************************************************
+
+import XCTest
+@testable import Calc
+
+class EnterNumberTests: XCTestCase {
+
+    // test if we can create long integer numbers in both sides
+
+}
