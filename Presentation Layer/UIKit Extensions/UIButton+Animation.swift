84bdf64d72add7b0424c00a8326acbbc7897697a
Matthew Paul Harding <matthewpharding@gmail.com>
2022-02-08 14:02:46 +0000

    Edited the UIButton animation to include easing and be a little nicer on the eyes.

--- Presentation Layer/UIKit Extensions/UIButton+Animation.swift
+++ Presentation Layer/UIKit Extensions/UIButton+Animation.swift
@@ -35,15 +35,14 @@ import UIKit
 extension UIButton {
     
     func bounce() {
-        UIView.animate(withDuration: 0.1,
-        animations: { [weak self] in
-        self?.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
-        self?.alpha = 1
-        },
-        completion: {  [weak self] _ in
-            UIView.animate(withDuration: 0.05) {  [weak self] in
+        
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self?.alpha = 1
+        } completion: { [weak self] _ in
+            UIView.animate(withDuration: 0.15, delay: 0, options: .curveEaseInOut, animations: { [weak self] in
                 self?.transform = CGAffineTransform.identity
-            }
-        })
+            }, completion: nil)
+        }
     }
 }
